6dab6995376080a3c2f91b2967085535c4d7cc8e
温酒 <zjwjxdm@gmail.com>
2017-12-29 14:27:42 +0800

    更新

---
+++ godot/P_2_Draw/1_Drawing/1_Drawing.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+    # Called every time the node is added to the scene.
+    # Initialization here
+    pass
+
+
+func draw_circle_arc_poly( center, radius, angle_from, angle_to, color ):
+    var nb_points = 32
+    var points_arc = Vector2Array()
+    points_arc.push_back(center)
+    var colors = ColorArray([color])
+
+    for i in range(nb_points+1):
+        var angle_point = angle_from + i*(angle_to-angle_from)/nb_points - 90
+        points_arc.push_back(center + Vector2( cos( deg2rad(angle_point) ), sin( deg2rad(angle_point) ) ) * radius)
+    draw_polygon(points_arc, colors)
+
+func _draw():
+    #your draw commands here
+    draw_circle ( Vector2(200,200), 200, Color(1,1,1,1) )
+    var center = Vector2(400,200)
+    var radius = 80
+    var angle_from = 75
+    var angle_to = 195
+    var color = Color(1.0, 0.0, 0.0)
+    draw_circle_arc_poly( center, radius, angle_from, angle_to, color )
+    pass
